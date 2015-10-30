4a7d429dc7974a209b3798f206d75121f5b61ac8
zhy <zhyage@yeah.net>
2015-10-30 14:39:15 +0800

    almost done with authentication

---
+++ dbConf/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+    'url' : 'mongodb://localhost/test'
+    //'url': 'mongodb://admin:admin@apollo.modulusmongo.net:27017/horyBo4r'
+}
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var methodConfHandle = require('../modules/methodConfHandle');
 var submit2Python = require('../modules/submit2Python');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+/*router.get('/', function(req, res, next) {
     res.sendfile('./views/index.html');
 });
 
@@ -59,6 +59,117 @@ router.post('/submitComputExpressAndData', function(req, res, next){
     }
 
     submit2Python.sendMsg2Py(submitBodyString, sendResBack, res);
-});
+});*/
 
-module.exports = router;
+
+
+var isAuthenticated = function(req, res, next) {
+    // if user is authenticated in the session, call the next() to call the next request handler 
+    // Passport adds this method to request object. A middleware is allowed to add properties to
+    // request and response objects
+    if (req.isAuthenticated())
+        return next();
+    // if the user is not authenticated then redirect him to the login page
+    res.redirect('/');
+}
+
+module.exports = function(passport) {
+
+    /* GET login page. */
+    router.get('/', function(req, res) {
+        // Display the Login page with any flash message, if any
+        res.render('index', {
+            message: req.flash('message')
+        });
+    });
+
+    /* Handle Login POST */
+    router.post('/login', passport.authenticate('login', {
+        successRedirect: '/home',
+        failureRedirect: '/',
+        failureFlash: true
+    }));
+
+    /* GET Registration Page */
+    router.get('/signup', function(req, res) {
+        res.render('register', {
+            message: req.flash('message')
+        });
+    });
+
+    /* Handle Registration POST */
+    router.post('/signup', passport.authenticate('signup', {
+        successRedirect: '/home',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
+
+    /* GET Home Page */
+    router.get('/home', isAuthenticated, function(req, res) {
+        res.render('first', { user: req.user });
+    });
+
+    router.get('/normalComputingPage', isAuthenticated, function(req, res, next) {
+        //res.sendfile('./views/normalComputing.html');
+        res.render('normalComputing', { user: req.user });
+    });
+
+    router.get('/biaoZhunHuaPage', isAuthenticated, function(req, res, next) {
+        //res.sendfile('./views/biaoZhunHua.html');
+        res.render('biaoZhunHua', { user: req.user });
+    });
+
+    router.get('/AHPConfPage1', isAuthenticated, function(req, res, next) {
+        //res.sendfile('./views/AHPConfPage1.html');
+        res.render('AHPConfPage1', { user: req.user });
+    });
+
+    router.post('/saveToServer', isAuthenticated, function(req, res, next) {
+        var saveInfo = req.body;
+        var fileName = saveInfo.fileName;
+        var matrixString = JSON.stringify(saveInfo.matrix);
+        res.send(saveAndLoad.saveMatrix(fileName, matrixString));
+    });
+
+    router.post('/loadFileFromServer', isAuthenticated, function(req, res, next) {
+        var loadInfo = req.body;
+        var fileName = loadInfo.fileName;
+        res.send(saveAndLoad.loadMatrix(fileName));
+    });
+
+    router.post('/getSaveFileList', isAuthenticated, function(req, res, next) {
+        res.send(saveAndLoad.getSavedFileList());
+    });
+
+    router.post('/toGetBiaoZhunFaConf', isAuthenticated, function(req, res, next) {
+        res.send(methodConfHandle.getPurifyMethodConf());
+    });
+
+    router.post('/toGetWeightFormulaConf', isAuthenticated, function(req, res, next) {
+        res.send(methodConfHandle.getAHPMethodConf());
+    });
+
+    router.post('/submitComputExpressAndData', isAuthenticated, function(req, res, next) {
+        var submitBody = req.body;
+
+        var submitBodyString = JSON.stringify(submitBody);
+
+
+        function sendResBack(res, resultMsg) {
+            console.info("here message back from python:", resultMsg);
+            res.send(resultMsg);
+        }
+
+        submit2Python.sendMsg2Py(submitBodyString, sendResBack, res);
+    });
+
+    /* Handle Logout */
+    router.get('/signout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    return router;
+}
+
+/*module.exports = router;*/
